54cfe0285030e9c99369219bafd39fff974077b4
royjl9292 <dakgalbi92@gmail.com>
2022-02-07 08:36:35 -0800

    Login and bluetooth test cases.

---
+++ app/src/test/java/edu/ucsd/cse110/lab5_room/loginTest.java
@@ -0,0 +1,53 @@
+package edu.ucsd.cse110.lab5_room;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import android.view.View;
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.HandlerThread;
+import android.os.ParcelUuid;
+import android.os.TestLooperManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import edu.ucsd.cse110.lab5_room.auth.LoginActivity;
+
+public class loginTest {
+/*
+    @Test
+    public void loginTest(){
+        LoginActivity loginActivity = new LoginActivity();
+
+
+    }
+*/
+    /*
+    Login test
+        case1: check users can put their first name.
+        case2: check if the next button is working.
+        case3: check when the users don't enter their firstname hit the next button.
+        case4: check what will happen when the users' first name is too long, and hit the next button.
+        case5: check if It can login with the google acct or not.
+        case6: check if users try to login with invalid google acct.
+        case7: check if users try to login with valid googl acct, but put incorrect passwords, many times.
+
+    Bluetooth test
+        case1: check the alert bar which asks uses are gonna turn on the blutooth in their settings or not.
+        case2: check if the NotNow button is working.
+        case3: check when the bluetooth is on and it can connect with other devices.
+        case4: check when the bluetooth is off and it shows "Bluetooth required" Messeage.
+        case5: check how many devices can connect with.
+
+
+
+     */
+}
