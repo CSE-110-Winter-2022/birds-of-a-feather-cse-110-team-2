4ecb9a7827ac086f25d85392a3dff782a4e866db
jongminkim <kingjm94@gmail.com>
2022-02-13 20:39:29 -0800

    previous classes / previous classes dao updated

--- app/src/main/java/edu/ucsd/cse110/lab5_room/model/db/previousClasses.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_room/model/db/previousClasses.java
@@ -1,4 +1,46 @@
 package edu.ucsd.cse110.lab5_room.model.db;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.List;
+
+@Entity (tableName = "previousClasses")
 public class previousClasses {
+    @PrimaryKey(autoGenerate = true)
+
+    @ColumnInfo(name = "previousClasses_id")
+    private int previousClassesId = 0;
+
+    @ColumnInfo(name = "previousClasses_list")
+    private List<String> previousClasses;
+
+    public previousClasses(List<String> previousClasses ) {
+        this.previousClasses = previousClasses;
+    }
+
+    public int getPhotoURLId() {
+        return previousClassesId;
+    }
+
+    public void setPreviousClassesId(int previousClassesId) {
+        this.previousClassesId = previousClassesId;
+    }
+
+    public List<String> getPreviousClasses() {
+        return this.previousClasses;
+    }
+
+    public void setPhotoURL(List<String> previousClasses) {
+        this.previousClasses = previousClasses;
+    }
+
+    public void insert(String course){
+        this.previousClasses.add(course);
+    }
+    public void delete(String course){
+        this.previousClasses.remove(course);
+    }
+
 }
--- app/src/main/java/edu/ucsd/cse110/lab5_room/model/db/previousClassesDao.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_room/model/db/previousClassesDao.java
@@ -1,5 +1,25 @@
 package edu.ucsd.cse110.lab5_room.model.db;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
 
-public class previousClassesDao {
+import java.util.List;
+
+@Dao
+public interface previousClassesDao {
+    @Transaction
+    @Query("SELECT * FROM previousClasses")
+    List<student> getAll();
+
+    @Query("SELECT * FROM previousClasses WHERE previousClasses_id=:id")
+    previousClasses get(int id);
+
+    @Insert
+    void insert(student s);
+
+    @Delete
+    void delete(student s);
 }
