faf4b76f1e39128eb83f4922defca4489d7b6611
Ganesh Susarla <ganeshsusarla01@gmail.com>
2022-02-13 19:23:26 -0800

    Added semi-working google login functionality (API Problems)

--- app/src/main/java/edu/ucsd/cse110/lab5_room/auth/LoginActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_room/auth/LoginActivity.java
@@ -5,7 +5,15 @@ import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import edu.ucsd.cse110.lab5_room.R;
 import edu.ucsd.cse110.lab5_room.internal.BoFButton;
@@ -14,11 +22,20 @@ import edu.ucsd.cse110.lab5_room.internal.CustomFilter;
 
 public class LoginActivity extends AuthActivity {
     private final String TAG = "LoginActivity";
-
+    private final int RC_SIGN_IN = 0;
+    Button googleBtn;
+    GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        googleBtn = (Button) findViewById(R.id.login_btn_google);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         checkBluetoothStatus(this);
 
@@ -30,10 +47,36 @@ public class LoginActivity extends AuthActivity {
 
         BoFButton nextButton = findViewById(R.id.login_btn_next);
         nextButton.trackEnable(firstName);
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()) {
+                    case R.id.login_btn_google:
+                        Intent signInIntent = googleSignInClient.getSignInIntent();
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                        break;
+                }
+            }
+        });
     }
 
-    public void onGoogleLoginClicked(View v) {
-        Log.d(TAG, "Google login clicked");
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.w(TAG, "signInResult:success " + account.getGivenName() + " " + account.getFamilyName());
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 
     public void onLoginNextClicked(View v) {
