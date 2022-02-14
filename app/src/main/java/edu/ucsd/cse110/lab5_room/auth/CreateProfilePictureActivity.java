245a0bd49bca440e4e8f6040002d5b2082263e1f
w1lai <w1lai@ucsd.edu>
2022-02-13 18:35:48 -0800

    Added Validator for photo's URL

--- app/src/main/java/edu/ucsd/cse110/lab5_room/auth/CreateProfilePictureActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_room/auth/CreateProfilePictureActivity.java
@@ -2,17 +2,24 @@ package edu.ucsd.cse110.lab5_room.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
 import edu.ucsd.cse110.lab5_room.MainActivity;
 import edu.ucsd.cse110.lab5_room.R;
+import edu.ucsd.cse110.lab5_room.Utilities;
 import edu.ucsd.cse110.lab5_room.internal.BoFButton;
 import edu.ucsd.cse110.lab5_room.internal.Constants;
 import edu.ucsd.cse110.lab5_room.internal.CustomFilter;
 
 public class CreateProfilePictureActivity extends AuthActivity {
 
+    private boolean validURL(EditText url) {
+        String urlString = url.getText().toString();
+
+        return Patterns.WEB_URL.matcher(urlString).matches();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +29,22 @@ public class CreateProfilePictureActivity extends AuthActivity {
         url.setFilters(new CustomFilter(Constants.CHARSET_URL).asSingleton());
 
         BoFButton nextBtn = findViewById(R.id.pfp_btn_next);
+
         nextBtn.trackEnable(url);
     }
 
     public void onProfilePicNextClicked(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(Constants.INTENT_LOGGED_IN, true);
-        startActivity(intent);
+
+        EditText urlPanel = findViewById(R.id.pfp_field_url);
+
+        if (validURL(urlPanel)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(Constants.INTENT_LOGGED_IN, true);
+            startActivity(intent);
+        }
+        else
+        {
+            Utilities.showAlert(this, "Please enter a valid URL");
+        }
     }
 }
