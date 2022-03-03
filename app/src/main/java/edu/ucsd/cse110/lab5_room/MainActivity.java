f40ba1dfd1633b7416dbc60f0fb06f5abfcc31fa
Ganesh Susarla <ganeshsusarla01@gmail.com>
2022-03-02 16:57:22 -0800

    Attempting to connect filter functions to UI using spinner dropdown box for user selection of filter

--- app/src/main/java/edu/ucsd/cse110/lab5_room/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_room/MainActivity.java
@@ -7,10 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.ucsd.cse110.lab5_room.auth.LoginActivity;
 import edu.ucsd.cse110.lab5_room.internal.Constants;
+import edu.ucsd.cse110.lab5_room.internal.StudentFilter;
 import edu.ucsd.cse110.lab5_room.model.DummyStudent;
 import edu.ucsd.cse110.lab5_room.model.Student;
 
@@ -21,10 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean start = true;
 
+    private Spinner filterMode;
+
     protected Student[] studentData = {
-            new DummyStudent("Alice", "https://www.wallpapers13.com/wp-content/uploads/2015/12/Nature-Lake-Bled.-Desktop-background-image-915x515.jpg", new String[]{"CSE 11"}, true, false),
-            new DummyStudent("Bob", "bob.net", new String[]{"ENG 3"}, false, false),
-            new DummyStudent("Carl", "https://www.bible-bridge.com/wp-content/uploads/favicon-256x256.png?x59487", new String[]{"CSE 101"}, true, false)
+            new DummyStudent("Alice", "https://www.wallpapers13.com/wp-content/uploads/2015/12/Nature-Lake-Bled.-Desktop-background-image-915x515.jpg", new String[]{"2022+Fall+CSE+11"}, true, true),
+            new DummyStudent("Bob", "bob.net", new String[]{"2022+Fall+ENG+3"}, false, false),
+            new DummyStudent("Carl", "https://www.bible-bridge.com/wp-content/uploads/favicon-256x256.png?x59487", new String[]{"2022+Spring+CSE+101"}, true, false)
     };
 
     @Override
@@ -40,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(this, LoginActivity.class);
             startActivity(i);
         }
+
+        filterMode = (Spinner)findViewById(R.id.filter_mode);
+
+        ArrayAdapter<CharSequence> filterAdapter = ArrayAdapter.createFromResource(this, R.array.filter_mode_array, android.R.layout.simple_spinner_item);
+        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterMode.setAdapter(filterAdapter);
+
 
         personsRecyclerView = findViewById(R.id.persons_view);
 
@@ -61,6 +77,44 @@ public class MainActivity extends AppCompatActivity {
                     //button.setBackgroundColor(R.color.pinkish);
                 }
                 start = !start;
+            }
+        });
+
+        filterMode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String currItem = adapterView.getItemAtPosition(i).toString();
+                Student[] dataToUse = studentData;
+                System.out.println(currItem);
+                if(currItem.equals("None")) {
+                    dataToUse = studentData;
+                }
+                else if(currItem.equals("Favorites")) {
+                    List<Student> filteredStudents = StudentFilter.filterStudents(studentData, StudentFilter.FilterType.FAVORITES, "");
+                    dataToUse = filteredStudents.toArray(new Student[filteredStudents.size()]);
+                }
+                else if (currItem.equals("Recent: Fall 2022")) {
+                    List<Student> filteredStudents = StudentFilter.filterStudents(studentData, StudentFilter.FilterType.CLASS_RECENT, "CLASS_RECENT?2022+Fall");
+                    dataToUse = filteredStudents.toArray(new Student[filteredStudents.size()]);
+                }
+
+                else if (currItem.equals("Recent: Spring 2022")) {
+                    List<Student> filteredStudents = StudentFilter.filterStudents(studentData, StudentFilter.FilterType.CLASS_RECENT, "CLASS_RECENT?2022+Spring");
+                    dataToUse = filteredStudents.toArray(new Student[filteredStudents.size()]);
+                }
+
+                else if (currItem.equals("Recent: Winter 2022")) {
+                    List<Student> filteredStudents = StudentFilter.filterStudents(studentData, StudentFilter.FilterType.CLASS_RECENT, "CLASS_RECENT?2022+Winter");
+                    dataToUse = filteredStudents.toArray(new Student[filteredStudents.size()]);
+                }
+                personsViewAdapter = new PersonsViewAdapter(dataToUse);
+                personsRecyclerView.setAdapter(personsViewAdapter);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
         //TODO: process out person who is not close
