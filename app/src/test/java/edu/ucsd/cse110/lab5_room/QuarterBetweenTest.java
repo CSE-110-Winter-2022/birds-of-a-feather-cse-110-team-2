6a7340989ba8793a07e25ee623ee6324b9b596fd
w1lai <w1lai@ucsd.edu>
2022-03-12 12:08:35 -0800

    fixing test issue

--- app/src/test/java/edu/ucsd/cse110/lab5_room/QuarterBetweenTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab5_room/QuarterBetweenTest.java
@@ -11,8 +11,8 @@ public class QuarterBetweenTest {
     @Test
     public void QuarterDifferenceIsCorrect()
     {
-        assertEquals(Course.quartersAgo(Course.Quarter.SPRING, 2021), 3);
-        assertEquals(Course.quartersAgo(Course.Quarter.WINTER, 2022), 0);
+        assertEquals(Course.quartersAgo(Course.Quarter.SP, 2021), 3);
+        assertEquals(Course.quartersAgo(Course.Quarter.WI, 2022), 0);
     }
 
 }
